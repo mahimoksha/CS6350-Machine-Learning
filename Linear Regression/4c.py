03f76559780a2202fda143f57550a73452b28e3d
Mokshagna <41355441+mahimoksha@users.noreply.github.com>
2022-10-21 11:56:03 -0600

    4c

---
+++ Linear Regression/4c.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+train_data = pd.read_csv("concrete/train.csv",header=None)
+
+X = train_data.iloc[:,:-1]
+Y = train_data.iloc[:,-1]
+X_train = np.column_stack(([1]*X.shape[0], X))
+X = np.array(X_train)
+Y = np.array(Y)
+
+print("Optimal weight vector that is calculated with the analytical form is : ",np.dot(np.linalg.inv(np.dot(X.T,X)),np.dot(X.T,Y)))
