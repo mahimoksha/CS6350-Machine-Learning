52b34f20dbc64cc4c52a2b61fe77e72acf62cdd5
Mokshagna <41355441+mahimoksha@users.noreply.github.com>
2022-10-21 20:10:57 -0600

    Update 4c.py

--- Linear Regression/4c.py
+++ Linear Regression/4c.py
@@ -10,3 +10,12 @@ X = np.array(X_train)
 Y = np.array(Y)
 
 print("Optimal weight vector that is calculated with the analytical form is : ",np.dot(np.linalg.inv(np.dot(X.T,X)),np.dot(X.T,Y)))
+
+
+
+X = train_data.iloc[:,:-1]
+Y = train_data.iloc[:,-1]
+X = np.array(X)
+Y = np.array(Y)
+
+print("Optimal weight vector that is calculated with the analytical form without bias feature is : ",np.dot(np.linalg.inv(np.dot(X.T,X)),np.dot(X.T,Y)))
