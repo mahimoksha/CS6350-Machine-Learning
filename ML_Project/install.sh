15ddca4310c33f9747ca9ddb5e323b040dac595b
Mokshagna <41355441+mahimoksha@users.noreply.github.com>
2022-12-19 14:14:05 -0700

    Add files via upload

---
+++ ML_Project/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e 
+set -x
+python3 -m pip install pandas
+python3 -m pip install numpy
+python3 -m pip install matplotlib
+python3 -m pip install tensorflow
+python3 -m pip install scikit-learn
+python3 -m pip install scipy
+python3 -m pip install xgboost
+python3 -m pip install seaborn
+python3 -m pip install tensorflow
---
+++ ML_Project/run_project.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+chmod +x install.sh
+./install.sh
+python3 ml_project.py
